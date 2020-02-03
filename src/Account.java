b81bbc29f47bb1a848d1512120888393536a6289
vadegg <vadegg0607@gmail.com>
2020-02-03 20:08:46 +0300

    alpha-ver. done.

--- src/Account.java
+++ src/Account.java
@@ -3,49 +3,46 @@ import java.util.function.DoubleBinaryOperator;
 
 public class Account {
 
-    private String type;
-    private String number;
-    private String currency;
-    private String operationDate;
-    private String reference;
+//    private String type;
+//    private String number;
+//    private String currency;
+//    private String operationDate;
+//    private String reference;
     private String description;
     private String in;
     private String out;
 
-    public Account(String type, String number, String currency,
-                   String operationDate, String reference,
-                   String description, String in, String out) {
-        this.type = type;
-        this.number = number;
-        this.currency = currency;
-        this.operationDate = operationDate;
-        this.reference = reference;
+    public Account(String description, String in, String out) { // пока что использую только 3 значения - операция, приход и расход, поэтому оставил только их
+//        this.type = type;
+//        this.number = number;
+//        this.currency = currency;
+//        this.operationDate = operationDate;
+//        this.reference = reference;
         this.description = description;
         this.in = in;
         this.out = out;
     }
-    public String getReference() {
-        return reference;
-    }
-
-    public void getOperationDate() {
-        System.out.println(operationDate);
-    }
+//    public String getReference() {
+//        return reference;
+//    }
+//
+//    public void getOperationDate() {
+//        System.out.println(operationDate);
+//    }
 
     public String getDescription() {
         String formattedDescription = null;
         int reverse = description.indexOf("/");
         int straight = description.indexOf("\\");
         if(reverse != -1) {
-            formattedDescription = description.substring(reverse+1, 70);
+            formattedDescription = description.substring(reverse+1);  // так как у ячеек в таблице фикс. длина, обрезаю сабстрингом
+            formattedDescription = formattedDescription.substring(0,41);
         }
         else if(straight != -1)
         {
-            formattedDescription = description.substring(straight+1, 70);
+            formattedDescription = description.substring(straight+1);
+            formattedDescription = formattedDescription.substring(0,41);
         }
-//        if (formattedDescription.length() > 41) {
-//            formattedDescription = formattedDescription.substring(42);
-//        }
         return formattedDescription;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.DecimalFormat;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        double sumIncome = 0;
+        DecimalFormat formatter = new DecimalFormat("###,###.###");
         List<String> lines = Files.readAllLines(Paths.get("data/movementList.csv"));
         ArrayList<Account> accounts = new ArrayList<>();
         lines.remove(0);
@@ -16,46 +15,38 @@ public class Main {
             String[] fragments = line.split(",");
             if (fragments.length == 8) {
                 accounts.add(new Account(
-                        fragments[0],
-                        fragments[1],
-                        fragments[2],
-                        fragments[3],
-                        fragments[4],
                         fragments[5],
                         fragments[6],
                         fragments[7]));
-            } else if (fragments.length == 9) {
+            } else if (fragments.length == 9) {  // проверка для того чтобы побороть кавычки в столбце расходов
                 String outcome = fragments[7].replaceAll("\"", "")
                         + "."
                         + fragments[8].replaceAll("\"", "");
                 accounts.add(new Account(
-                        fragments[0],
-                        fragments[1],
-                        fragments[2],
-                        fragments[3],
-                        fragments[4],
                         fragments[5],
                         fragments[6],
                         outcome));
             }
         }
-
+        /**---------==========------Для суммирования расходов создал HashMap-------------------------*/
         Map<String, Double> outcomes = new HashMap<String, Double>();
         for (Account account : accounts) {
+            sumIncome += account.getIncome();
             if (!outcomes.containsKey(account.getDescription())) {
                 outcomes.put(account.getDescription(), account.getOutcome());
             }
             if (outcomes.containsKey(account.getDescription())) {
                 if(!outcomes.containsValue(account.getOutcome()))
-                outcomes.put(account.getDescription(), (outcomes.get(account.getDescription()).doubleValue()
+                outcomes.put(account.getDescription(), (outcomes.get(account.getDescription())
                                 + account.getOutcome()));
             }
         }
+        System.out.println("========================================================================================");
+        System.out.println("Всего поступило на счет - " + formatter.format(sumIncome) + " руб.");
+        System.out.println("========================================================================================");
+        System.out.println("Расходы : ");
         for (Map.Entry entry : outcomes.entrySet()) {
-            System.out.println(entry.getKey() + "------ " + entry.getValue() + " руб.");
+            System.out.println(entry.getKey() + "- " + formatter.format(entry.getValue()) + " руб.");
         }
-
-
-
     }
 }
